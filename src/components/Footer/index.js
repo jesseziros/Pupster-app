0e5b0a95e4c979daf40c8dc717eff7c6d24dba19
Jesse Ziros <jtziros2020@gmail.com>
2022-01-09 20:48:09 -0500

    Jesse branch (#3)
    * deleted logo and added folders
    
    * Created files in Card and CardBtn, modified App.js
    
    * Completed Design of Website, Started pages folder and Created axios call in utils folder.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span> Pupster 2022</span>
+        </footer>
+    );
+}
+
+export default Footer;
